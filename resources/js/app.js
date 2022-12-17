22859c7f1d564f578911372dabcfc386d07bcaca
Polin Wei <polin.wei@gmail.com>
2022-12-17 10:32:44 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+/** ninestars: Vendor JS Files & Template Main JS File
+import '../ninestars/vendor/aos/aos.js';
+import '../ninestars/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import '../ninestars/vendor/glightbox/js/glightbox.min.js';
+import '../ninestars/vendor/isotope-layout/isotope.pkgd.min.js';
+import '../ninestars/vendor/swiper/swiper-bundle.min.js';
+import '../ninestars/vendor/php-email-form/validate.js';
+import '../ninestars/js/main.js';
+ */
+//import.meta.glob(['../ninestars/**']);
